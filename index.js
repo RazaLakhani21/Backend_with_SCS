76d47d13e4f50e01af5536ee121a6e82e16c8a78
Lakhani Mohammad Raza <razalakhani21@gmail.com>
2025-08-19 12:26:49 +0530

    Learning theory about node_modules, Package, Package-lock.json and used 'Cat-me' package

---
+++ index.js
@@ -0,0 +1,16 @@
+// console.log("Hello World!");
+
+// for (let i=0; i<10; i++) {
+//     console.log(i);
+// }
+
+const catMe = require('cat-me')
+function sum(a, b){
+    return a + b
+}
+
+var summation = sum(12, 34)
+
+console.log("Sum of 12 and 34 is", summation)
+
+// console.log(catMe())
