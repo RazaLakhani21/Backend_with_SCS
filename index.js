b51328813be6fd6a00ad18b0d2ed5a5fc03c8bff
Lakhani Mohammad Raza <razalakhani21@gmail.com>
2025-09-08 12:00:58 +0530

    Learning to create server and it's Instance

--- index.js
+++ index.js
@@ -4,13 +4,24 @@
 //     console.log(i);
 // }
 
-const catMe = require('cat-me')
-function sum(a, b){
-    return a + b
-}
+// const catMe = require('cat-me')
+// function sum(a, b){
+//     return a + b
+// }
 
-var summation = sum(12, 34)
+// var summation = sum(12, 34)
 
-console.log("Sum of 12 and 34 is", summation)
+// console.log("Sum of 12 and 34 is", summation)
 
 // console.log(catMe())
+
+
+const http = require('http')
+
+const server = http.createServer((req, res)=>{
+    console.log(req.url)
+    res.end('Hello World')
+})
+
+server.listen(3000)
+
