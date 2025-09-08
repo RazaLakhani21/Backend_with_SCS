acd83afd5e4dbf83afa1de2156b3b5d39cbb1da0
Lakhani Mohammad Raza <razalakhani21@gmail.com>
2025-09-08 18:12:01 +0530

    Learning about Middleware more, created custom Middleware and also used third-party middleware, moving forward to create middleware for individual route

--- app_express.js
+++ app_express.js
@@ -1,7 +1,30 @@
 const express = require('express');
+
+// Third-Party Middleware 👇
+const morgan = require('morgan'); // morgan is a logger, which shows when the request came to your server, how much time it took?, of which method the requests wereand etc..
+
 const app = express();
 
+app.use(morgan('dev'))
+
 app.set("view engine", 'ejs')
+
+// Custom Middleware
+app.use((req,res,next)=>{
+    console.log("This is Middleware")
+
+    const a = 2
+    const b = 3
+    
+    console.log(a+b)
+
+    return next()
+})
+
+// There are 3 types of Middlewares:
+// 1. Inbuilt Middleware
+// 2. Custom Middleware
+// 3. Third-Party Middleware
 
 app.get('/', (req, res) => {
     // res.send("Hello World hi world ban rahe hain")
