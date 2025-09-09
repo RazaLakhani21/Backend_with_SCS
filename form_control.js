35f4bed177a1f85462e6ed1b38f1d8cf2c229bb5
Lakhani Mohammad Raza <razalakhani21@gmail.com>
2025-09-09 12:36:22 +0530

    Learning about 'post' method and used other 2 middlewares, now moving forward on linking css with the server

---
+++ form_control.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.set("view engine", 'ejs');
+
+app.get('/', (req, res)=>{
+    res.send('This is the Home Page');
+})
+
+app.get('/form', (req, res)=>{
+    res.render('form');
+})
+
+// not to show any private information like "Password" in the url, we'll use 'app.post' instead of 'app.get'
+
+// app.get('/get-form-data', (req, res)=>{
+app.post('/get-form-data', (req, res)=>{
+    // console.log(req.query);
+    console.log(req.body);
+    res.send('Data Recieved')
+})
+
+app.listen(3000)
