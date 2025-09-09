b45db3c60dca77eabc011f67316239a2e02e1d58
Lakhani Mohammad Raza <razalakhani21@gmail.com>
2025-09-09 07:16:02 +0530

    Learn about Middlewares and created some of the middlewares using third-party app and custom middlewares also, Moving Forward to Built-In Controller and Form-Controller(how to control forms).

--- app_express.js
+++ app_express.js
@@ -1,42 +1,60 @@
-const express = require('express');
+const express = require("express");
 
 // Third-Party Middleware 👇
-const morgan = require('morgan'); // morgan is a logger, which shows when the request came to your server, how much time it took?, of which method the requests wereand etc..
+const morgan = require("morgan"); // morgan is a logger, which shows when the request came to your server, how much time it took?, of which method the requests wereand etc..
 
 const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
-app.set("view engine", 'ejs')
+app.set("view engine", "ejs");
 
 // Custom Middleware
-app.use((req,res,next)=>{
-    console.log("This is Middleware")
+// app.use((req,res,next)=>{
+//     console.log("This is Middleware")
 
-    const a = 2
-    const b = 3
-    
-    console.log(a+b)
+//     const a = 2
+//     const b = 3
 
-    return next()
-})
+//     console.log(a+b)
+
+//     return next()
+// })
 
 // There are 3 types of Middlewares:
 // 1. Inbuilt Middleware
 // 2. Custom Middleware
 // 3. Third-Party Middleware
 
-app.get('/', (req, res) => {
+// Generating Custom Middleware in the already generated '/' using 'get' tool of express.
+// Middleware only for '/' route.
+
+app.get(
+  "/",
+
+
+  (req, res, next) => {
+    const a = 5;
+    const b = 10;
+    console.log(a + b);
+    next();
+  },
+
+
+  (req, res) => {
     // res.send("Hello World hi world ban rahe hain")
-    res.render("index")
-})
+    res.render("index");
+  }
 
-app.get('/about', (req, res) => {
-    res.send("About Page")
-})
+  
+);
 
-app.get('/profile', (req, res)=>{
-    res.send("Profile Page")
-})
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
 
-app.listen(3000)
+app.get("/profile", (req, res) => {
+  res.send("Profile Page");
+});
+
+app.listen(3000);
